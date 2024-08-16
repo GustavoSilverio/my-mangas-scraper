655b708a3a6fb3a357d76c77d4c4f4d3903c6b58
GustavoSilverio <gustavosk290905@gmail.com>
2024-08-16 16:29:41 -0300

    multi-threading

--- webScrapping.py
+++ webScrapping.py
@@ -1,71 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
-from typing import List
+from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urlparse
 
-def obterCapitulos(nome_manga: str):
+def obterCapitulos(url_manga: str):
+   
+    # obter cada página de cada capítulo
+    
+    def obterPaginasDoCapitulo(link_capitulo: str):
+        result_capitulo = requests.get(link_capitulo)
+        if result_capitulo.status_code != 200:
+            return None
+        
+        soup_capitulo = BeautifulSoup(result_capitulo.text, "html.parser")
+        paginas = soup_capitulo.find("div", class_="content").find_all("p")[1:-1]
+        lista_paginas = [p.img["src"] for p in paginas if p.img and "src" in p.img.attrs]
+        
+        parsed_url_capitulo = urlparse(link_capitulo).path
+        numero_capitulo = parsed_url_capitulo.split("/")[-2].split("capitulo")[-1][1:].replace("-", ".")
+        capitulo_manga = f"Capítulo {numero_capitulo}"
+        
+        return lista_paginas, capitulo_manga
+    
+    
     # obter lista de capítulos do manga
-    result_capitulos = requests.get(nome_manga)
-
+    
+    result_capitulos = requests.get(url_manga)
     if result_capitulos.status_code != 200:
         print("Página do mangá não encontrada!")
+        return [], ""
 
     soup_capitulos = BeautifulSoup(result_capitulos.text, "html.parser")
-
     lista_capitulos_html = soup_capitulos.find_all("div", class_="episodiotitle")
-
-    lista_links_capitulos : List[str] = [] 
-
-    for cap in lista_capitulos_html:
-        if cap.a['href']:
-            lista_links_capitulos.append(cap.a['href'])
-        else:
-            print("Capitulo sem link!")
-
+    lista_links_capitulos = [cap.a['href'] for cap in lista_capitulos_html if cap.a['href']]
     print("Capítulos encontrados: ", len(lista_links_capitulos))
 
-    # obter cada página de cada capítulo
+    with ThreadPoolExecutor() as executor:
+        lista_capitulos = []
 
-    lista_capitulos : List[List[str]] = []
-    lista_paginas : List[str] = []
-    contador_capitulos = 1
-    contador_paginas = 1
-    contador_erros = 0
-
-    for link_cap in reversed(lista_links_capitulos): # reversed pois os capitulos começam do último
+        pools = [executor.submit(obterPaginasDoCapitulo, link_cap) for link_cap in lista_links_capitulos]
         
-        result_capitulo = requests.get(link_cap)
-        
-        if result_capitulo.status_code != 200:
-            print("Página do capítulo não encontrada!")
-            continue
-        
-        else:
-            soup_capitulo = BeautifulSoup(result_capitulo.text, "html.parser")
-            paginas = soup_capitulo.find("div", class_="content").find_all("p")[1:-1]
-            
-            for p in paginas:
-                    if p.img and "src" in p.img.attrs:
-                        lista_paginas.append(p.img["src"])
-                        contador_paginas += 1
-                    else:
-                        contador_erros += 1
-                        lista_paginas.append(None)
-                        
-            
-            parsed_url_capitulo = urlparse(link_cap).path
-            numero_capitulo = parsed_url_capitulo.split("/")[-2].split("capitulo")[-1][1:].replace("-", ".")
-            
-            lista_capitulos.append({
-                    "nomeCapitulo": f"Capítulo {numero_capitulo}",
-                    "paginas": lista_paginas
+        for pool in pools:
+            paginas, nome_capitulo  = pool.result()
+            if paginas and nome_capitulo is not None:
+                lista_capitulos.append({
+                    "nomeCapitulo": nome_capitulo,
+                    "paginas": paginas
                 })
-            
-            contador_capitulos += 1
-            contador_paginas = 1
-            lista_paginas = []
-    
+
     nome_manga = soup_capitulos.title.text.split(" - ")[0]
+    print("Páginas não encontradas: ", sum(1 for res in lista_capitulos if res is None))
     
-    print("Páginas não encontradas: ", contador_erros)
     return lista_capitulos, nome_manga
